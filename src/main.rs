796010bd6baee7b8e60ca4fac6740494990f1b0a
Patrick Dubroy <pdubroy@gmail.com>
2024-04-18 12:41:22 +0200

    Finish chap 7: filled triangles

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ use std::fmt;
 use std::thread::sleep;
 use std::time::Duration;
 
+mod math;
+use math::{interpolate, Point};
+
 const WIDTH: usize = 800;
 const HEIGHT: usize = 600;
 
 fn main() {
     let mut canvas = Canvas::new(WIDTH, HEIGHT);
-
 
     let mut window = Window::new(
         "Test - ESC to exit",
@@ -28,8 +30,11 @@ fn main() {
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         canvas.fill(0x008080);
-        canvas.draw_wireframe(&p1, &p2, &p3, 0xFFFFFF);
-        window.update_with_buffer(&canvas.data, WIDTH, HEIGHT).unwrap();
+        // canvas.draw_wireframe(&p1, &p2, &p3, 0xFFFFFF);
+        canvas.draw_filled_triangle(&p1, &p2, &p3, 0xFFFFFF);
+        window
+            .update_with_buffer(&canvas.data, WIDTH, HEIGHT)
+            .unwrap();
 
         update_point_and_velocity(&mut p1, &mut v1);
         update_point_and_velocity(&mut p2, &mut v2);
@@ -52,18 +57,6 @@ fn update_point_and_velocity(p: &mut Point, v: &mut Point) {
     if p.y.unsigned_abs() as usize >= hh {
         v.y *= -1;
         p.y += v.y;
-    }
-}
-
-#[derive(Debug)]
-struct Point {
-    pub x: i32,
-    pub y: i32
-}
-
-impl Point {
-    fn new(x: i32, y: i32) -> Point {
-        Point { x, y }
     }
 }
 
@@ -104,23 +97,16 @@ impl Canvas {
     //     self.data[y_norm as usize * self.width + x_norm as usize]
     // }
 
+    #[allow(dead_code)]
     fn draw_line(&mut self, p0: &Point, p1: &Point, color: u32) {
         if (p1.x - p0.x).abs() > (p1.y - p0.y).abs() {
-            let (p0, p1) = if p0.x > p1.x {
-                (p1, p0)
-            } else {
-                (p0, p1)
-            };
+            let (p0, p1) = if p0.x > p1.x { (p1, p0) } else { (p0, p1) };
             let ys = interpolate(p0.x, p0.y as f32, p1.x, p1.y as f32);
             for x in p0.x..p1.x + 1 {
                 self.set_pixel(x, ys[(x - p0.x) as usize] as i32, color);
             }
         } else {
-            let (p0, p1) = if p0.y > p1.y {
-                (p1, p0)
-            } else {
-                (p0, p1)
-            };
+            let (p0, p1) = if p0.y > p1.y { (p1, p0) } else { (p0, p1) };
             let xs = interpolate(p0.y, p0.x as f32, p1.y, p1.x as f32);
             for y in p0.y..p1.y + 1 {
                 self.set_pixel(xs[(y - p0.y) as usize] as i32, y, color);
@@ -128,11 +114,54 @@ impl Canvas {
         }
     }
 
+    #[allow(dead_code)]
     fn draw_wireframe(&mut self, p0: &Point, p1: &Point, p2: &Point, color: u32) {
         self.draw_line(p0, p1, color);
         self.draw_line(p1, p2, color);
         self.draw_line(p2, p0, color);
-        // self.draw_line(x0, y0, x2, y2, color);
+    }
+
+    fn draw_filled_triangle(&mut self, p0: &Point, p1: &Point, p2: &Point, color: u32) {
+        let mut p0 = p0;
+        let mut p1 = p1;
+        let mut p2 = p2;
+
+        // Sort the points so that y0 <= y1 <= y2
+        if p1.y < p0.y {
+            std::mem::swap(&mut p1, &mut p0);
+        }
+        if p2.y < p0.y {
+            std::mem::swap(&mut p2, &mut p0);
+        }
+        if p2.y < p1.y {
+            std::mem::swap(&mut p2, &mut p1);
+        }
+
+        // Compute the x coordinates of the triangle edges
+        let mut x01 = interpolate(p0.y, p0.x as f32, p1.y, p1.x as f32);
+        let x12 = interpolate(p1.y, p1.x as f32, p2.y, p2.x as f32);
+        let x02 = interpolate(p0.y, p0.x as f32, p2.y, p2.x as f32);
+
+        // Concatenate the short sides
+        x01.pop();
+        let x012 = [x01, x12].concat();
+
+        // Determine which is left and which is right
+        let m = x012.len() / 2;
+        let mut x_left = x02;
+        let mut x_right = x012;
+        if x_right[m] < x_left[m] {
+            std::mem::swap(&mut x_left, &mut x_right);
+        }
+
+        // Draw the horizontal segments
+        for y in p0.y..p2.y + 1 {
+            let x_start = x_left[(y - p0.y) as usize] as i32;
+            let x_end = x_right[(y - p0.y) as usize] as i32;
+            for x in x_start..x_end + 1 {
+                self.set_pixel(x, y, color)
+            }
+        }
     }
 }
 
@@ -140,48 +169,119 @@ impl fmt::Display for Canvas {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut s = String::new();
         for y in 0..self.height {
-            let pixels: Vec<_> = (0..self.width).map(|x| {
-                let px = self.data[y * self.width + x];
-                if px == 0 { "-"} else { "X" }
-            }).collect();
+            let pixels: Vec<_> = (0..self.width)
+                .map(|x| {
+                    let px = self.data[y * self.width + x];
+                    if px == 0 {
+                        "-"
+                    } else {
+                        "X"
+                    }
+                })
+                .collect();
             s.push_str(&pixels.join(" "));
-            if y + 1 != self.height { s.push('\n'); }
+            if y + 1 != self.height {
+                s.push('\n');
+            }
         }
         write!(f, "{}", s)
     }
 }
 
-// Convention: values of the independent variable i are always integers, as
-// they represent pixels, while the values of the dependent variable d
-// are always floating point values, as they represent values of a generic
-// linear function.
-fn interpolate(i0: i32, d0: f32, i1: i32, d1: f32) -> Vec<f32> {
-    if i0 == i1 {
-        return vec![d0];
-    }
-    let mut values = Vec::new();
-    let a = (d1 - d0) / (i1 as f32 - i0 as f32);
-    let mut d = d0;
-    for _ in i0..i1 + 1 {
-        values.push(d);
-        d += a;
-    }
-    values
-}
-
 #[cfg(test)]
-mod tests {
+mod test {
     use super::*;
+
+    fn canvas_with_filled_triangle(p1: &Point, p2: &Point, p3: &Point) -> Canvas {
+        let mut canvas = Canvas::new(3, 3);
+        canvas.draw_filled_triangle(&p1, &p2, &p3, 0xFFFFFF);
+        canvas
+    }
+
     #[test]
-    fn test_lines() {
+    fn test_draw_line() {
         let mut canvas = Canvas::new(5, 5);
         canvas.draw_line(&Point::new(-2, 2), &Point::new(0, -2), 0xFFFFFF);
-        assert_eq!(canvas.to_string(), "
+        assert_eq!(
+            canvas.to_string(),
+            "
 X - - - -
 - X - - -
 - X - - -
 - - X - -
 - - X - -
-        ".trim());
+        "
+            .trim()
+        );
+    }
+
+    #[test]
+    fn test_draw_filled_triangle() {
+        let canvas =
+            canvas_with_filled_triangle(&Point::new(-1, 1), &Point::new(0, 0), &Point::new(-1, -1));
+        assert_eq!(
+            canvas.to_string(),
+            "
+X - -
+X X -
+X - -
+        "
+            .trim()
+        );
+
+        let p1 = Point::new(-1, 1);
+        let p2 = Point::new(1, 1);
+        let p3 = Point::new(-1, -1);
+        let canvas = canvas_with_filled_triangle(&p1, &p2, &p3);
+        assert_eq!(
+            canvas.to_string(),
+            "
+X X X
+X X -
+X - -
+        "
+            .trim()
+        );
+
+        assert_eq!(
+            canvas_with_filled_triangle(&p1, &p2, &p3).to_string(),
+            canvas_with_filled_triangle(&p1, &p3, &p2).to_string()
+        );
+        assert_eq!(
+            canvas_with_filled_triangle(&p1, &p2, &p3).to_string(),
+            canvas_with_filled_triangle(&p3, &p1, &p2).to_string()
+        );
+        assert_eq!(
+            canvas_with_filled_triangle(&p1, &p2, &p3).to_string(),
+            canvas_with_filled_triangle(&p3, &p2, &p1).to_string()
+        );
+
+        let canvas = canvas_with_filled_triangle(&p1, &p1, &p1);
+        assert_eq!(
+            canvas.to_string(),
+            "
+X - -
+- - -
+- - -
+        "
+            .trim()
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_filled_triangle_corner_cases() {
+        let canvas =
+            canvas_with_filled_triangle(&Point::new(-1, 1), &Point::new(0, 1), &Point::new(1, 1));
+        // Due to the special case in `interpolate`, this will not produce the expected result.
+        assert_eq!(
+            canvas.to_string(),
+            "
+X X X
+- - -
+- - -
+        "
+            .trim()
+        );
     }
 }
---
+++ src/math.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+pub struct Point {
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Point {
+    pub fn new(x: i32, y: i32) -> Point {
+        Point { x, y }
+    }
+}
+
+// Convention: values of the independent variable i are always integers, as
+// they represent pixels, while the values of the dependent variable d
+// are always floating point values, as they represent values of a generic
+// linear function.
+pub fn interpolate(i0: i32, d0: f32, i1: i32, d1: f32) -> Vec<f32> {
+    if i0 == i1 {
+        return vec![d0];
+    }
+    let mut values = Vec::new();
+    let a = (d1 - d0) / (i1 as f32 - i0 as f32);
+    let mut d = d0;
+    for _ in i0..i1 + 1 {
+        values.push(d);
+        d += a;
+    }
+    values
+}
